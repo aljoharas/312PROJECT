85e28fb78e6135d932fcfbe093aced3628dc8a5a
judychickadee <judyabed22@gmail.com>
2024-11-15 22:22:42 +0300

    Cart updates

--- script.js
+++ script.js
@@ -2,7 +2,9 @@
 
     /* Checkout Message */
         function checkout() {
-            alert("Thank you for your purchase!");
+            var total = document.getElementById("totalCost").innerHTML;
+            alert("Thank you for your purchase! \nYour total is: " + total);
+            window.location.href="Product evaluation.html";
         }
 
     /* Quantity Buttons */
@@ -15,12 +17,11 @@
                 var price = parseInt(parent.parentNode.querySelector(".price").innerHTML.replace("$", ""));
                 var subTotal = (quant-1) * price;
                 parent.parentNode.querySelector(".subtotal").innerHTML = "$" + subTotal;
-                var subTotal = parseInt(document.getElementById("finalSubtotal").innerHTML.replace("$","")) - price;
-                document.getElementById("finalSubtotal").innerHTML = "$" + subTotal;
             }
             else {
-                parent.parentNode.parentNode.remove();
+                parent.parentNode.parentNode.remove(); 
             }
+            updatePrice();
         }
 
         // Increase 
@@ -31,13 +32,17 @@
             var price = parseInt(parent.parentNode.querySelector(".price").innerHTML.replace("$", ""));
             var subTotal = quant * price;
             parent.parentNode.querySelector(".subtotal").innerHTML = "$" + subTotal;
-            var subTotal = parseInt(document.getElementById("finalSubtotal").innerHTML.replace("$","")) + price;
-            document.getElementById("finalSubtotal").innerHTML = "$" + subTotal;
+            updatePrice();
         }
 
     /* Remove */
     function removeItem(button) {
         button.parentNode.parentNode.parentNode.remove();
+        var items = document.getElementsByClassName("item");
+        if (items.length == 0){
+            document.getElementById("shippingCost").innerHTML = "$0"  
+        }
+        updatePrice();
     }
 
     /* Empty Cart */
@@ -47,10 +52,42 @@
         for (var i = items.length - 1; i >= 0; i--) {
             items[i].remove();
         }
-        document.getElementById("totalCost").innerHTML = "$0";
-        document.getElementById("finalSubtotal").innerHTML = "$0";
         document.getElementById("shippingCost").innerHTML = "$0"
+        updatePrice();
     }
 
+    /* Update total price */
+    function updatePrice(){
+        var subTotal = 0;
+        var total = 0;
+        var items = document.getElementsByClassName("item");
+        for (var i=items.length -1 ; i >=0 ; i--){
+            subTotal += parseInt(items[i].querySelector(".subtotal").innerHTML.replace("$",""));
+            total += parseInt(items[i].querySelector(".subtotal").innerHTML.replace("$",""));
+        }
+        total += parseInt(document.getElementById("shippingCost").innerHTML.replace("$",""));
+        document.getElementById("finalSubtotal").innerHTML = "$" + subTotal;
+        document.getElementById("totalCost").innerHTML = "$" + total;
+        discount();
+    }
 
+    /* Continue Shopping */
+    function shop(){
+        window.location.href="homepage.html";
+    }
+
+    /* Apply Discount */
+    /*function discount(){
+        var discount = document.getElementById("discountbox").value;
+        if (discount.startsWith("TTL")){
+            var discountPercent = parseFloat(discount.substring(3));
+            var total = parseInt(document.getElementById("totalCost").innerHTML.replace("$",""))
+            alert(discountPercent);
+            var discountedAmount = total * (discountPercent/100);
+            var newTotal = total - discountedAmount;
+            document.getElementById("totalCost").innerHTML = "$" + newTotal;
+        }
+    }*/
+
+        
 /* ###################### INSERT NEXT PAGE NAME ###################### */
