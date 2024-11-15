515d7ec700e133a1a5dac8ecff9cfba53ea59612
judychickadee <judyabed22@gmail.com>
2024-11-15 17:36:34 +0300

    Quantity Counter Javascript

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
 /* ###################### CART ###################### */
 
-    /* Quantity Box */
-    function decreaseQuantity(){
-        document.getElementsByClassName('decrease');
+    /* Checkout Message */
+    function checkout(){
+        alert("Thank you for your purchase!");
     }
+
+    /* Quantity Buttons */
+        // Decrease 
+        function decrease(button){
+            var parent = button.parentNode;
+            var quant = parseInt(parent.querySelector(".quantity").value);
+            if (quant > 1){
+                parent.querySelector(".quantity").value = quant - 1;
+            }
+            else {
+                parent.parentNode.parentNode.remove();
+            }
+        }
+
+        // Increase 
+        function increase(button){
+            var parent = button.parentNode;
+            var quant = parseInt(parent.querySelector(".quantity").value) + 1;
+            parent.querySelector(".quantity").value = quant;
+        }
